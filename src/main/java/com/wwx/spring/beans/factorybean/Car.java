5c99b3d3807a7a36b6ec7cd35435fab61c9804f0
wuwenxi <1139541228@qq.com>
2018-05-06 22:20:54 +0800

    spring基础

---
+++ src/main/java/com/wwx/spring/beans/factorybean/Car.java
@@ -0,0 +1,40 @@
+package com.wwx.spring.beans.factorybean;
+
+public class Car {
+
+    private String name;
+    private Integer price;
+
+    public Car() {
+    }
+
+    public Car(String name, Integer price) {
+        this.name = name;
+        this.price = price;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "name='" + name + '\'' +
+                ", price=" + price +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
